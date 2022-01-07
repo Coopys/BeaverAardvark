7d80554b9bb30b3762c0b2fef8b0ad5e1a0306b2
csb62 <csb62@DESKTOP-G2E7BM5>
2022-01-07 12:04:11 -0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("aardvark or beaver");
+
+             string   j = Console.ReadLine();
+            
+
+
+
+            if (j.Contains("beaver") == true)
+            {
+                Console.WriteLine(@"
+
+
+
+
+                                          A                      
+             ___                         / \
+          .=     =._.---.              _/   \             ______________
+                  c  Y p \           /     \    \
+       /     \     `\  w_ /            :~~~~~~~: |      /  / ) -   .   -
+ jgs |      )  /     /              |       |    |     <  )      --
+______ |     / __ -\ \_ =.\              |       |     \   \ )  - -
+  (XXXXX / `------)))=-___________ / __ / __\___\___ _  \)___________
+ ~~~~~
+
+
+
+
+
+
+
+                                                                                              ");
+                Console.ReadLine();
+
+
+
+
+            }
+            else if (j.Contains("aardvark") == true)
+            {
+                Console.WriteLine(@" 
+                    _,,......_
+                 ,-'          `'--.
+              ,-'  _              '-.
+     (`.    ,'   ,  `-.              `.
+      \ \  -    / )    \               \
+       `\`-^^^, )/      |     /         :
+         )^ ^ ^V/            /          '.
+         |      )            |           `.
+         9   9 /,--,\    |._:`         .._`.
+         |    /   /  `.  \    `.      (   `.`.
+         |   / \  \    \  \     `--\   )    `.`.___
+-hrr-   .;;./  '   )   '   )       ///'       `-"
+        
+
+
+
+
+
+
+
+
+
+                                                            );
+                Console.ReadLine();
+
+
+            }
+
+
+        }
+    }
+}
